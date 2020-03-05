cc4d0883233a56c8768686052035c61081af017d
Sil van Diepen <me@silvandiepen.com>
2020-03-05 08:14:33 +0100

    don't forget files

---
+++ src/components/grid/grid.vue
@@ -0,0 +1,24 @@
+<template></template>
+<script>
+export default {
+	computed: {
+		gridClasses() {
+			return ["kudt"];
+		}
+	},
+	props: {
+		small: {
+			type: String,
+			default: null
+		},
+		medium: {
+			type: String,
+			default: null
+		},
+		large: {
+			type: String,
+			default: null
+		}
+	}
+};
+</script>
